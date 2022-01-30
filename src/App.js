2e3b6fb1e84a1a8bcb1355429c5c07c9cef797ce
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-30 21:34:00 +0200

    Fetch Data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+import { useEffect, useState } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+
 const url = 'https://course-api.com/react-tours-project';
 function App() {
-	return <h2>Tours Project Setup</h2>;
+	const [data, setData] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState(false);
+
+	useEffect(() => {
+		setIsLoading(true);
+		try {
+			const getData = async () => {
+				const res = await fetch('https://course-api.com/react-tours-project');
+				const tours = await res.json();
+				setData(tours);
+			};
+			getData();
+			setIsLoading(false);
+			setIsError(false);
+		} catch (error) {
+			setIsLoading(false);
+			setIsError(error);
+		}
+	}, []);
+
+	return (
+		<>
+			{isLoading && <Loading />}
+			{isError && { isError }}
+			{data.length > 0 && <Tours tours={data} />}
+		</>
+	);
 }
 
 export default App;
