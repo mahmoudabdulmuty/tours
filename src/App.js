a5352d54b122e2fc07e8a15da9363c07d3e729c1
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-31 13:34:09 +0200

    fix : RefreshBtn is shown only if all tours deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useCallback, useEffect, useState } from 'react';
 import Error from './Error';
 import Loading from './Loading';
+import RefreshBtn from './RefreshBtn';
 import Tours from './Tours';
 
 function App() {
 	const [tours, setTours] = useState([]);
-	const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
 
 	const handleDelete = (id) => {
@@ -17,14 +18,12 @@ function App() {
 	};
 
 	const fetchTours = useCallback(async () => {
-		setLoading(true);
 		try {
 			const res = await fetch('https://course-api.com/react-tours-project');
 			if (!res.ok) throw new Error(res.statusText);
 			const tours = await res.json();
-			setTours(tours);
 			setLoading(false);
-			setError(false);
+			setTours(tours);
 		} catch (error) {
 			setLoading(false);
 			setError("can't fetch tours");
@@ -38,18 +37,11 @@ function App() {
 
 	return (
 		<main>
+			{tours.length > 0 && <Tours handleDelete={handleDelete} tours={tours} />}
 			{loading && <Loading />}
 			{error && <Error error={error} />}
-			{tours.length > 0 && <Tours handleDelete={handleDelete} tours={tours} />}
-			{!error && tours.length === 0 && (
-				<>
-					<div className="title">
-						<h2>no tours left</h2>
-						<button className="btn" onClick={fetchTours}>
-							refresh
-						</button>
-					</div>
-				</>
+			{!error && !loading && tours.length === 0 && (
+				<RefreshBtn fetchTours={fetchTours} />
 			)}
 		</main>
 	);
