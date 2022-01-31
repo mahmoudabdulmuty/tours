113f73ee89d68b5cc567df3fe47c1a26eaf60995
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-31 12:01:08 +0200

    fix : fixed useState variable names

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import { useEffect, useState } from 'react';
+import Error from './Error';
 import Loading from './Loading';
 import Tours from './Tours';
 
-const url = 'https://course-api.com/react-tours-project';
 function App() {
 	const [data, setData] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [isError, setIsError] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
+
+	const fetchTours = async () => {
+		setLoading(true);
+		try {
+			const res = await fetch(
+				'https://jsonplaceholder.typicode.com/todossssss'
+			);
+			if (!res.ok) throw new Error(res.statusText);
+			const tours = await res.json();
+			setData(tours);
+			setLoading(false);
+			setError(false);
+		} catch (error) {
+			setLoading(false);
+			setError("can't fetch data");
+			console.log(error.message);
+		}
+	};
 
 	useEffect(() => {
-		setIsLoading(true);
-		try {
-			const getData = async () => {
-				const res = await fetch('https://course-api.com/react-tours-project');
-				const tours = await res.json();
-				setData(tours);
-			};
-			getData();
-			setIsLoading(false);
-			setIsError(false);
-		} catch (error) {
-			setIsLoading(false);
-			setIsError(error);
-		}
+		fetchTours();
 	}, []);
 
 	return (
 		<main>
-			{isLoading && <Loading />}
-			{isError && { isError }}
+			{loading && <Loading />}
+			{error && <Error error={error} />}
 			{data.length > 0 && <Tours tours={data} />}
 		</main>
 	);
